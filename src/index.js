655928ef7fb9c96e5bae15d924b8943bce242bbb
ivan.auchynnikau <ivan.auchynnikau@itechart-group.com>
2020-10-11 01:11:51 +0300

    add home page patyials

--- src/index.js
+++ src/index.js
@@ -1,11 +1,18 @@
 // default styles
-import './assets/fonts/fonts.css';
 import './assets/styles/reset.css';
-
 
 // component styles
 import './assets/common.css';
 import './partials/main-section/main-section.css';
+import './partials/what-we-have/what-we-have.css';
+import './partials/who-we-are/who-we-are.css';
+import './partials/contact-us/contact-us.css';
+import './partials/enjoy-us/enjoy-us.css';
+import './partials/footer/footer.css';
+import './partials/header/header.css';
+import './partials/section-title/section-title.css';
+import './partials/form/form.css';
+import './partials/seo-section/seo-section.css';
 
 // js
 import './partials/got-to-top-btn/go-to-top-btn';
