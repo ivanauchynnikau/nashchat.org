2142b4ef8e2b02c6488cd2219955c3220e9da598
ivan.auchynnikau <ivan.auchynnikau@itechart-group.com>
2020-10-16 21:39:57 +0300

    rebuild prod

--- src/partials/terms-conditions/terms-conditions.js
+++ src/partials/terms-conditions/terms-conditions.js
@@ -2,26 +2,52 @@ $(document).ready(() => {
   const termsTitle = '[data-selector="terms-title"]';
 
   if (!$(termsTitle).length) return;
-
-  const termsSection = '[data-selector="terms-section"]';
-  const termsContent = '[data-selector="terms-content"]';
   const activeClassName = '_active';
+  const termsContent = '[data-selector="terms-content"]';
 
-  // show/hide terms page content
-  $(document).on('click', termsTitle, (event) => {
-    const $currentTitle = $(event.target);
-    const $currentSection = $currentTitle.closest(termsSection);
-    const $currentContent = $currentSection.find(termsContent);
+  function toggleSection(sectionId) {
+    const $termsSection = $(`#${sectionId}`);
+    const $currentContent = $termsSection.find(termsContent);
 
-    if ($currentSection.hasClass(activeClassName)) {
-      $currentContent.slideUp(300, () => {
-        $currentSection.removeClass(activeClassName);
+    if ($termsSection.hasClass(activeClassName)) {
+      $currentContent.slideUp(700, () => {
+        $termsSection.removeClass(activeClassName);
       });
       return;
     }
 
-    $currentContent.slideDown(300, () => {
-      $currentSection.addClass(activeClassName);
+    $currentContent.slideDown(700, () => {
+      $termsSection.addClass(activeClassName);
     });
+  }
+
+  function scrollIntoView(id) {
+    const { top } = $(id).offset();
+    $('body,html').animate({ scrollTop: top }, 1500);
+  }
+
+  function onPageLoad() {
+    if (window.location.hash === '#policy') {
+      toggleSection('policy');
+      scrollIntoView('#policy');
+    }
+
+    if (window.location.hash === '#terms') {
+      toggleSection('terms');
+      scrollIntoView('#terms');
+    }
+  }
+
+  onPageLoad();
+
+  // show/hide terms page content
+  $(document).on('click', termsTitle, (event) => {
+    const $currentTitle = $(event.target);
+    const termsSection = '[data-selector="terms-section"]';
+    const $currentSection = $currentTitle.closest(termsSection);
+    const sectionId = $currentSection.attr('id');
+    toggleSection(sectionId);
   });
+
+  window.addEventListener('hashchange', onPageLoad, false);
 });
